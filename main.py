d43d8a52ff3de8de54908a9e96ed582d1f35b0bf
anjali <anjali0907ch@gmail.com>
2022-07-12 13:39:21 +0530

    report adde

--- main.py
+++ main.py
@@ -15,6 +15,35 @@ filename = 'nlp_model.pkl'
 clf = pickle.load(open(filename, 'rb'))
 vectorizer = pickle.load(open('tranform.pkl','rb'))
     
+def create_similarity():
+    data = pd.read_csv('main_data.csv')
+    # creating a count matrix
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(data['comb'])
+    # creating a similarity score matrix
+    similarity = cosine_similarity(count_matrix)
+    return data,similarity
+
+def rcmd(m):
+    m = m.lower()
+    try:
+        data.head()
+        similarity.shape
+    except:
+        data, similarity = create_similarity()
+    if m not in data['movie_title'].unique():
+        return('Sorry! The movie you requested is not in our database. Please check the spelling or try with some other movies')
+    else:
+        i = data.loc[data['movie_title']==m].index[0]
+        lst = list(enumerate(similarity[i]))
+        lst = sorted(lst, key = lambda x:x[1] ,reverse=True)
+        lst = lst[1:11] # excluding first item since it is the requested movie itself
+        l = []
+        for i in range(len(lst)):
+            a = lst[i][0]
+            l.append(data['movie_title'][a])
+        return l
+    
 # converting list of string to list (eg. "["abc","def"]" to ["abc","def"])
 def convert_to_list(my_list):
     my_list = my_list.split('","')
@@ -27,7 +56,7 @@ def convert_to_list_num(my_list):
     my_list = my_list.split(',')
     my_list[0] = my_list[0].replace("[","")
     my_list[-1] = my_list[-1].replace("]","")
-    return my_list
+    return my_list    
 
 def get_suggestions():
     data = pd.read_csv('main_data.csv')
@@ -41,6 +70,15 @@ def home():
     suggestions = get_suggestions()
     return render_template('home.html',suggestions=suggestions)
 
+@app.route("/similarity",methods=["POST"])
+def similarity():
+    movie = request.form['name']
+    rc = rcmd(movie)
+    if type(rc)==type('string'):
+        return rc
+    else:
+        m_str="---".join(rc)
+        return m_str
 
 @app.route("/recommend",methods=["POST"])
 def recommend():
